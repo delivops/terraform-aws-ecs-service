fd7f0a89324e5bf11c9e0de9d1d9d20849a8b262
osnatashush <osnat@delivops.com>
2025-05-05 12:03:26 +0300

    fix bugs

--- variables.tf
+++ variables.tf
@@ -203,9 +203,9 @@ variable "schedule_auto_scaling" {
   type = object({
     enabled = optional(bool, false)
     schedules = optional(list(object({
-      schedule_name       = optional(number,"")
-      min_replicas        = optional(number,1)
-      min_replicas        = optional(number,1)
+      schedule_name       = optional(string, "")
+      min_replicas        = optional(number, 1)
+      max_replicas        = optional(number, 1)
       schedule_expression = optional(string, "cron(0 0 1 * ? *)") # cron expression
       time_zone           = optional(string, "Asia/Jerusalem")
     })), [])
